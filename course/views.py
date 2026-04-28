c9aeda414a47177bff95ef1209fa1adf3b1fc4cd
Arbinda-Sahu <arbindakumarsahu13@gmail.com>
2026-04-28 21:10:13 +0530

    Refactor submit and show_exam_result functions

--- course/views.py
+++ course/views.py
@@ -1,51 +1,41 @@
-from django.shortcuts import render
-from .models import Question, Choice, Submission
+from django.shortcuts import render, redirect
+from .models import Course, Enrollment, Question, Choice, Submission
 
 
-def submit(request):
-    if request.method == 'POST':
-        user_name = request.POST.get('user_name')
-        total = 0
-        score = 0
+def submit(request, course_id):
+    course = Course.objects.get(id=course_id)
+    enrollment = Enrollment.objects.first()
 
-        questions = Question.objects.all()
+    submission = Submission.objects.create(
+        enrollment=enrollment
+    )
 
-        for question in questions:
-            selected_choice_id = request.POST.get(str(question.id))
-            if selected_choice_id:
-                choice = Choice.objects.get(id=selected_choice_id)
+    for question in course.question_set.all():
+        selected_choice_id = request.POST.get(str(question.id))
+        if selected_choice_id:
+            choice = Choice.objects.get(id=selected_choice_id)
+            submission.choice_set.add(choice)
 
-                Submission.objects.create(
-                    user_name=user_name,
-                    question=question,
-                    selected_choice=choice
-                )
-
-                total += 1
-                if choice.is_correct:
-                    score += 1
-
-        context = {
-            'score': score,
-            'total': total
-        }
-
-        return render(request, 'course/result.html', context)
+    return redirect('show_exam_result', course_id=course.id, submission_id=submission.id)
 
 
-def show_exam_result(request):
-    submissions = Submission.objects.all()
+def show_exam_result(request, course_id, submission_id):
+    course = Course.objects.get(id=course_id)
+    submission = Submission.objects.get(id=submission_id)
 
-    total = submissions.count()
-    correct = 0
+    total_score = 0
+    possible_score = 0
 
-    for sub in submissions:
-        if sub.selected_choice.is_correct:
-            correct += 1
+    for question in course.question_set.all():
+        possible_score += 1
+        if question.is_get_score(submission):
+            total_score += 1
 
     context = {
-        'score': correct,
-        'total': total
+        'course': course,
+        'submission': submission,
+        'total_score': total_score,
+        'possible_score': possible_score
     }
 
-    return render(request, 'course/result.html', context)
+    return render(request, 'course/exam_result_bootstrap.html', context)
